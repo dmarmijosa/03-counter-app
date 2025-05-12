89eca65c23592dfaf22230faa019e9311a4177d9
Danny Armijos <daniel2667@hotmail.es>
2025-05-12 23:27:36 +0200

    Implementa contador básico con botones para incrementar y reiniciar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(10);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textHuge}>{count}</Text>
+      <Pressable
+        style={styles.floatingBotton}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+      >
+        <Text style={{ color: "white", fontSize: 20 }}>+1</Text>
+      </Pressable>
+      {/* <TouchableOpacity>
+        <Text>-1</Text>
+      </TouchableOpacity> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  textHuge: {
+    fontSize: 120,
+    fontWeight: "100",
+  },
+
+  floatingBotton: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    backgroundColor: "#65558F",
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    elevation: 3,
+    shadowRadius: 5,
   },
 });
