a77745ca49d42b2e6b25fc210d5e64ed4a71cac7
Danny Armijos <daniel2667@hotmail.es>
2025-05-13 00:18:16 +0200

    Refactor: mejora el estilo del componente FAB para manejar la opacidad al presionar

--- components/FAB.tsx
+++ components/FAB.tsx
@@ -16,9 +16,10 @@ export default function FAB({
 }: Props) {
   return (
     <Pressable
-      style={[
+      style={({ pressed }) => [
         styles.floatingBotton,
         position === "right" ? styles.positionRight : styles.positionLeft,
+        pressed ? { opacity: 0.7 } : { opacity: 1 },
       ]}
       onPress={onPress}
       onLongPress={onLongPress}
